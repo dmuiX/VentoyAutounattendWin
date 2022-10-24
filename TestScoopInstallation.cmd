12a8bdcfe062380624a52d38fc80d447605caa16
dmuiX <store@posteo.de>
2022-10-24 16:53:20 +0200

    So this is how it works: Scoop installation must be run without admin rights or you can use a special command to run it with admin rights Haven't found a clue to run a non admin shell from cmd

--- TestScoopInstallation.cmd
+++ TestScoopInstallation.cmd
@@ -17,9 +17,15 @@ set log=%systemdrive%\%~n0.log
 
 :: install Scoop
 	::download install.ps1
-%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/ScoopInstaller/Install/master/install.ps1','%SYSTEMDRIVE%\install.ps1'))"
+::Powershell -NoProfile -ExecutionPolicy Bypass -Command "((iex (new-object net.webclient).downloadstring('https://get.scoop.sh')))"
+:: %systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "((new-object net.webclient).DownloadFile('https://raw.githubusercontent.com/ScoopInstaller/Install/master/install.ps1','%SYSTEMDRIVE%\install.ps1'))"
+:: This is working but limited to non admin users!
+:: powershell -ExecutionPolicy Bypass -File %SYSTEMDRIVE%\install.ps1 >> %log%
+
 	::run installer
-%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%SYSTEMDRIVE%\install.ps1 > %SYSTEMDRIVE%\scoop_install.log' %*"
+:: Installation of scoop cannot be run with admin rights instead you can use this:
+%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "((iex "& {$(irm get.scoop.sh)} -RunAsAdmin"))"
+::%systemroot%\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& 'irm get.scoop.sh | iex' %*"
 ::&& ($LASTEXITCODE -eq 0) ? Write-Output >Scoop was installed correctly" >> $ENV:SYSTEMDRIVE/scoop_install.log  : Write-Output "Scoop was not installed correctly" >> $ENV:SYSTEMDRIVE/scoop_install.log
 :: push tag to stop
 ::echo stop > %systemroot%\DONE_SDI.tag
