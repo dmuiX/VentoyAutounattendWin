6de94572b06efe8ad6981b85da97eb2dae77572a
Ullrich, Daniel <daniel.ullrich@scandio.de>
2023-02-26 23:15:55 +0100

    Choco: - Deleted choco-cleaner, treesizefree, logitech-options - Replaced logitech-unifying with unifying
    ventoy:
    Added Option: "VTOY_SORT_CASE_SENSITIVE": "1"

--- SetupComplete/Scripts/choco.cmd
+++ SetupComplete/Scripts/choco.cmd
@@ -5,9 +5,7 @@ set log=%systemdrive%\%~n0.log
 choco feature enable -n allowEmptyChecksums
 choco feature enable -n allowGlobalConfirmation
 choco install sudo -y -r %log%
-choco install clink-maintained -y -r %log%
 choco install steam -y -r %log%
-choco install choco-cleaner -y -r %log%
 choco install firefox -y -r %log%
 choco install microsoft-windows-terminal -y -r %log%
 choco install hashtab -y -r %log%
@@ -17,14 +15,11 @@ choco install itunes -y -r %log%
 choco install mediainfo -y -r %log%
 choco install advanced-renamer -y -r %log%
 choco install vscode -y -r %log%
-choco install treesizefree -y -r %log%
 choco install defraggler -y -r %log%
 choco install vmwareworkstation -y -r %log%
 choco install mpc-be -y -r %log%
-choco install logitech-unifying -y -r %log%
-choco install logitech-options -y -r %log%
+choco install unifying -y -r %log%
 choco install teracopy -y -r %log%
 choco install partitionwizard -y -r %log%
 choco install git -y -r %log%
-choco install choco-cleaner -y -r %log%
 )
